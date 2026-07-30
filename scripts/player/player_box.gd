0d3f31c883c6ab96bdcf1badba53776d8aa3e4f0
KevinRabbe <rabbekevin@icloud.com>
2026-07-30 05:45:09 +0200

    Give player health and death handling

--- scripts/player/player_box.gd
+++ scripts/player/player_box.gd
@@ -1,10 +1,32 @@
-extends Node2D
+extends StaticBody2D
 
+signal died
+
+@onready var health_component: HealthComponent = $HealthComponent
 @onready var targeting_component: TargetingComponent = $TargetingComponent
 @onready var weapon_component: WeaponComponent = $WeaponComponent
 
 
+func _ready() -> void:
+	health_component.died.connect(_on_died)
+
+
 func _process(_delta: float) -> void:
+	if health_component.is_dead():
+		return
+
 	var target := targeting_component.find_nearest_target(global_position)
 	if target != null:
 		weapon_component.try_fire(self, target)
+
+
+func take_damage(amount: float) -> void:
+	health_component.take_damage(amount)
+
+
+func get_health_component() -> HealthComponent:
+	return health_component
+
+
+func _on_died() -> void:
+	died.emit()
